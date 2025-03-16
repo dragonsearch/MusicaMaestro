2c2bf3dd8a592091b43ce0856978002bb0d8e224
dragonsearch <javmalla1@gmail.com>
2025-03-16 01:16:36 +0100

    update iscached for both events and commands

--- src/loaders/events/functions/iscached.js
+++
@@ -1,7 +0,0 @@
-function iscached(file){
-    if (require.cache[require.resolve(`../../../events/${file}`)] != undefined){
-        console.log(`File event ${file} is cached`);
-        delete require.cache[require.resolve(`../../../events/${file}`)];
-    };
-};
-module.exports = {iscached}
