757ceeac26a403f98a8c46f24a78e605f1be5660
dragonsearch <javmalla1@gmail.com>
2025-04-27 01:36:27 +0200

    fix interactionCreate not working with queue buttons

--- src/events/interactionCreate.mjs
+++ src/events/interactionCreate.mjs
@@ -1,4 +1,9 @@
 export const name = "interactionCreate";
+//LEGACY, TODO: RECHECK LOGIC
 export async function run(bot, interaction) {
+  if (interaction.client.commands.get(interaction.commandName)) {
     interaction.client.commands.get(interaction.commandName).run(interaction);
+  } else {
+    console.log("Interaction not in the command list", interaction);
+  }
 }
