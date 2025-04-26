0840b23a4df3d57e8089ddaef6a6427f68d398d3
dragonsearch <javmalla1@gmail.com>
2025-04-27 01:34:37 +0200

    feat: add queues and items in play command

--- src/commands/play.mjs
+++ src/commands/play.mjs
@@ -25,7 +25,7 @@ export const options = [
 ];
 
 export async function run(interaction) {
-    // Should do an instant reply so interaction doesn't time out
+  // Should do an instant reply so interaction doesn't time out
   await interaction.reply({ content: "Playing audio...", ephemeral: true });
   let url = interaction.options.getString("yt_url");
   let player = interaction.client.audio_player;
@@ -76,62 +76,24 @@ export async function run(interaction) {
 
     let queue = interaction.client.queues.get(interaction.guild.id);
     if (!queue) {
-        queue = new Queue(player, null);
-        interaction.client.queues.set(interaction.guild.id, queue);
+      queue = new Queue(player, null);
+      interaction.client.queues.set(interaction.guild.id, queue);
     }
     let extractor = new Yt_dlp_Extractor();
     let items = [];
-    try{
-        items = await extractor.getItems(url);
+    try {
+      items = await extractor.getItems(url);
+      console.log("queueing");
+      queue.enqueue(items);
+      queue.emit("play");
     } catch (error) {
-        console.log('Error:', error.message);
-        await interaction.reply({ content: 'Error: ' + error.message, ephemeral: true });
-        return;
+      console.log("Error:", error.message);
+      await interaction.followUp({
+        content: "Error: " + error.message,
+        ephemeral: true,
+      });
+      return;
     }
-    queue.enqueue(items);
-    queue.emit('play');
-    player.on('error', error => {
-        queue.replay();
-        console.log('Error:', error.message);
-    })
-    let connection = new InteractionConnection(interaction);
-    console.log('Creating connection');
-    let connected = connection.createConnection();
-    console.log('Connection created');
-    //const voice_connection = getVoiceConnection(interaction.guild.id);
-    //let audio = await add_audio(player,interaction);
-    console.log('Got voice connection');
-
-    if (connected) {
-        console.log('Playing audio');
-        //play_sound(voice_connection, audio);
-
-
-        //let re = new RegExp('[\\/]'); // No backslashes or forwardslashes allowed in file names
-        //let name = audio;
-        const connection = getVoiceConnection(interaction.guild.id);
-        try {
-            await entersState(connection, VoiceConnectionStatus.Ready, 10000);
-            console.log("Connected: " );
-        } catch (error) {
-            console.log("Voice Connection not ready within 10s.", error);
-            return null;
-        }
-        if (!connection) {
-            await interaction.reply({ content: 'No connection :(', ephemeral: true });
-            return;
-        }
-
-        //const resource = createAudioResource(audio); // This uses the working directory as the root folder (where the bot is running)
-        connection.subscribe(player);
-
-        
-    
-    }
-    return;
-    
-
-
+  }
+  return;
 }
-
-
