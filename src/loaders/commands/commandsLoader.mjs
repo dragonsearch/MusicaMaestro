9cc525e6c691c4b5aeaf3c8ee8465fe3a6aa9e7c
dragonsearch <javmalla1@gmail.com>
2025-04-27 14:08:26 +0200

    feat: global (multi-guild) commands

--- src/loaders/commands/commandsLoader.mjs
+++ src/loaders/commands/commandsLoader.mjs
@@ -1,24 +1,22 @@
-import 'dotenv/config';
-import discord from 'discord.js';
-import { REST, Routes } from 'discord.js';
+import "dotenv/config";
+import discord from "discord.js";
+import { REST, Routes } from "discord.js";
 
-import { getSaveCommands } from './functions/getCommands.mjs';
-const rest = new REST({version: '10'}).setToken(process.env.TOKEN);
+import { getSaveCommands } from "./functions/getCommands.mjs";
+const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
 
-
-async function loadCommands(bot){
-    //Get save commands returns a promise so we need to await it
-    let commandslist = await getSaveCommands(bot)
-    try {
-        console.log('Started refreshing application (/) commands.');
-        await rest.put(
-            Routes.applicationGuildCommands(process.env.CLIENT_ID,process.env.GUILD_ID),
-            {body: commandslist},
-        );
-        console.log('Successfully reloaded application (/) commands.');
-    }catch(error){
-        console.error(error);
-    }
+async function loadCommands(bot) {
+  //Get save commands returns a promise so we need to await it
+  let commandslist = await getSaveCommands(bot);
+  try {
+    console.log("Started refreshing application (/) commands.");
+    await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), {
+      body: commandslist,
+    });
+    console.log("Successfully reloaded application (/) commands.");
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-export default loadCommands
+export default loadCommands;
